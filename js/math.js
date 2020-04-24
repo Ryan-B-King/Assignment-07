9ba668639108b94978bb12744d0ebafffc4db643
KING-PC\King <ryan13king@gmail.com>
2020-04-23 23:34:04 -0700

    updated assignment

--- js/math.js
+++ js/math.js
@@ -2,10 +2,47 @@
 
 //STEP 1
 
+// let num = window.prompt('Please input a positive or negative number.');
+
+// if (isNaN(num) || num === '') {
+//     window.console.log('That is not a valid number.  Try again.');
+// } else {
+//     window.console.log('The absolute value of your number is: ' + Math.abs(num));
+// };
+
 //STEP 2
+
+// let floatNum = window.prompt('Input a decimal number.');
+
+// if (isNaN(floatNum) || floatNum === '') {
+//     window.console.log('That is not a valid number.  Try again.');
+// } else {
+//     window.console.log('Your number ' + floatNum + ' has been rounded up to the nearest whole number of: ' + Math.ceil(floatNum));
+// };
 
 //STEP 3
 
+// let floatNum = window.prompt('Input a decimal number.');
+
+// if (isNaN(floatNum) || floatNum === '') {
+//     window.console.log('That is not a valid number.  Try again.');
+// } else {
+//     window.console.log('Your number ' + floatNum + ' has been rounded down to the nearest whole number of: ' + Math.floor(floatNum));
+// };
+
 //STEP 4
 
+// let numbers = window.prompt('Input 5 numbers each separated by commas(,).');
+// window.console.log('Your numbers are: ' + numbers + '.');
+// let num1 = numbers.match(/\d{1,}/g)[0];
+// let num2 = numbers.match(/\d{1,}/g)[1];
+// let num3 = numbers.match(/\d{1,}/g)[2];
+// let num4 = numbers.match(/\d{1,}/g)[3];
+// let num5 = numbers.match(/\d{1,}/g)[4];
+
+// window.console.log('The smallest number you provided is: ' + Math.min(num1, num2, num3, num4, num5) + '.\nThe largest number you provided is: ' + Math.max(num1, num2, num3, num4, num5) + '.');
+
 //STEP 5
+
+// let num = window.prompt('Input a # to find its square root.');
+// window.console.log('The square root of ' + num + ' is ' + Math.sqrt(num) + '.');
--- js/strings.js
+++ js/strings.js
@@ -2,20 +2,98 @@
 
 //STEP 1
 
+// const name = window.prompt('What is your name?');
+// window.alert('Your name has the character length of ' + name.length + '.');
+
 //STEP 2
+
+// function nameInput() {
+//     name = window.prompt('What is your name?');
+//     if (name.length === 0) {
+//         window.alert('Sorry you did not enter a valid name.  Try Again.');
+//         nameInput();
+//     } else {
+//         return name;
+//     };
+// };
+
+// function nameChar() {
+//     char = Number(window.prompt(name + ' has the character length of ' + name.length + '.\n' + 
+//                          'Pick a # from 1-' + name.length + '.'));
+//     if (char === 'null' || char > name.length || char <= 0) {
+//         window.alert('Sorry you did not enter a valid #.  Try Again.');
+//         while (char === 'null' || char > name.length || char <= 0) {
+//             nameChar() ;
+//         };
+//     } else {
+//         return char;
+//     };
+// };
+
+// function nameAlert() {
+//     charNumber =  name.charAt(char-1);
+//     window.alert('The # you selected represents the letter: ' + charNumber);
+// };
+
+// function main() {
+//     let name;
+//     let char;
+//     let charNumber;
+
+//     nameInput();
+//     nameChar();
+//     nameAlert();
+// };
+// main();
 
 //STEP 3
 
+// let fName = window.prompt('What is your first name?');
+// let lName = ' ' +  window.prompt('What is your last name?');
+// window.alert('Your name is: ' + fName.concat(lName) + '.');
+
 //STEP 4
+
+// let text = 'The quick brown fox jumps over the lazy dog';
+// window.alert(text.indexOf('fox'));
 
 //STEP 5
 
+// let text = 'The quick brown fox jumps over the lazy fox';
+// window.alert(text.lastIndexOf('fox'));
+
 //STEP 6
+
+// let text = 'The quick brown fox jumps over the lazy dog';
+// let name = window.prompt('What is your full name?');
+// window.alert(text.replace('the lazy dog', name));
 
 //STEP 7
 
+// let text = 'The quick brown fox jumps over the lazy dog';
+// let input = window.prompt('Choose a word from: ' + text);
+
+
+// if (text.search(input) === -1 || input === '') {
+//     window.alert('Input is invalid. Try again.');
+// } else {
+//     window.alert(text.search(input));
+// };
+
 //STEP 8
+
+// let old_string = 'The quick brown fox jumps over the lazy dog';
+// let new_string = old_string.slice(30, 43);
+// window.alert(new_string.toUpperCase());
 
 //STEP 9
 
+// let text = '     THE QUICK BRONW FOX JUMPS OVER THE LAZY DOG     ';
+// window.alert(text.toLowerCase().trim());
+
 //STEP 10
+
+// let text = 'the quick brown fox jumps over the lazy dog';
+// let text2 = text.slice(1, 43);
+// let text3 = text.slice(0, 1).toUpperCase();
+// window.alert(text3.concat(text2));
